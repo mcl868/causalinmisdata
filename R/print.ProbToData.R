03ba31840dc878d8accf7168f7defdc1ccefae51
Thomas Maltesen <atmaltesen@gmail.com>
2019-11-14 15:25:35 +0100

    Change

--- R/print.ProbToData.R
+++ R/print.ProbToData.R
@@ -2,9 +2,17 @@ print.ProbToData<-function(x, digits=3, ...){
   object<-x
   rm(x)
   if(inherits(object,"ProbToData")){
-  
+ 
   cat("Missing pattern is",tolower(object$pattern),"and the coeffiecnts are of the regression are:\n")
-  if(object$pattern=="Monotone"){for(jj in 1:length(object$CoefList)){print(round(object$CoefList[[jj]],digits))}}
+  if(object$pattern=="Monotone"){
+     for(jj in 1:length(object$CoefList)){
+       if(is.null(object$CoefList[[jj]])){
+         NULL
+       } else {
+         print(round(object$CoefList[[jj]],digits))
+       }
+     }
+  }
   if(object$pattern=="TwoLevel"){print(round(object$CoefList,digits))}
 
   cat("Data:\n")
