4016fc44f2de02a8b080bb3337dc0909fc17678c
Thomas Maltesen <atmaltesen@gmail.com>
2020-02-21 11:32:14 +0100

    Update g.dicho.formula.R

--- R/g.dicho.formula.R
+++ R/g.dicho.formula.R
@@ -3,6 +3,9 @@ g.dicho.formula<-function(mmodels, exposure, data, complete = FALSE, ...){
   len.m.mod<-length(mmodels)
   len.expo<-length(exposure)
 
+  for(i in 1:(len.m.mod-1)){eval(parse(text=paste0("mmodels[[i+1]]<-update(mmodels[[i+1]], model",i," ~ .)")))}
+  rm("i")
+
   if(complete){data<-na.omit(data)}
 
   out<-list()
@@ -18,13 +21,15 @@ g.dicho.formula<-function(mmodels, exposure, data, complete = FALSE, ...){
       mvalues<-predict(glm(mmodels[[ii_]], data = data),type="response",newdata=confounderexposure)
       eval(parse(text=paste0("data$model",ii_,"<-mvalues")))
     }
-    rm("confounderexposure")
+    rm(list=c("confounderexposure","ii_"))
 
   eval(parse(text=paste0("out$Y_",paste0(EXPOmat[j,],collapse=""),"<-mvalues")))
 
   estimate[j,]<-mean(eval(parse(text=paste0("data$model",length(mmodels)))))
   estname[j]<-paste0("EY",paste0(EXPOmat[j,],collapse=""))
   }
+  rm("j")
+
   rownames(estimate)<-estname
   out$estimate<-estimate
 
