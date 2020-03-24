e2086ed229600f471ddb553f8d36dd22bfdef14c
Thomas Maltesen <atmaltesen@gmail.com>
2020-03-24 12:54:42 +0100

    Update modelcomponent.R

--- R/modelcomponent.R
+++ R/modelcomponent.R
@@ -1,6 +1,6 @@
 modelcomponent<-function(expo, ...){
   len.expo<-length(expo)
 
-  c(1,unlist(
+  c(1,unique(unlist(
       sapply(1:(len.expo-1), function(i) combiexpo(combn(expo,i)))),
-    paste0(combn(expo,len.expo),collapse="*"))}
+    paste0(combn(expo,len.expo),collapse="*")))}
