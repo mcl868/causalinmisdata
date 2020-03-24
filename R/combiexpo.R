d25555c4f5112d1c74e94c05d7339517fac5aa85
Thomas Maltesen <atmaltesen@gmail.com>
2020-03-24 13:25:18 +0100

    update

--- R/combiexpo.R
+++ R/combiexpo.R
@@ -1,3 +1,2 @@
 combiexpo<-function(expo, ...){
-  expo<-matrix(expo)
   sapply(1:ncol(expo), function(i)paste0(expo[,i],collapse="*"))}
--- R/modelcomponent.R
+++ R/modelcomponent.R
@@ -1,6 +1,7 @@
 modelcomponent<-function(expo, ...){
   len.expo<-length(expo)
 
-  c(1,unique(unlist(
-      sapply(1:(len.expo-1), function(i) combiexpo(combn(expo,i)))),
-    paste0(combn(expo,len.expo),collapse="*")))}
+  if(len.expo>1)
+    c(1,unlist(
+        sapply(1:(len.expo-1), function(i) combiexpo(combn(expo,i)))),
+      paste0(combn(expo,len.expo),collapse="*")) else c(1,expo)}
